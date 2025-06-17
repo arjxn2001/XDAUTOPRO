41341c344c174dfe6e1fceee1c87ebc984bf50f4
arjxn2001 <kbarjun12@gmail.com>
2025-06-18 03:30:00 +0530

    deploy

--- src/Components/InsideNav.jsx
+++ src/Components/InsideNav.jsx
@@ -41,12 +41,12 @@ const InsideNav = () => {
         <img src={logo} alt="XD logo" className='navLogo w-30 h-10' />
       </div>
 
-        <ul className='ulItem flex lg:gap-20 md:gap-13 gap-6 text-black  text-xs items-center'>
+        <ul className='ulItem flex lg:gap-20 md:gap-13 gap-6 text-black   items-center  '>
                 {["home", "services", "contact"].map((id) => (
                     <li
                     key={id}
                     onClick={() => handleClick(id)}
-                    className={`cursor-pointer border-b-2 transition-all duration-200 ${
+                    className={` cursor-pointer border-b-2 transition-all duration-200 ${
                         active === id ? "border-black" : "border-transparent"
                     }`}
                     >
